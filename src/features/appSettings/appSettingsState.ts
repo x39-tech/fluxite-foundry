9861c3ad89768f1f644bfcf699626f9e155e095b
Sam Kearney <samuel.kearney@taittowers.com>
2022-09-14 15:15:17 -0600

    initialize dark mode from browser default

--- src/features/appSettings/appSettingsState.ts
+++ src/features/appSettings/appSettingsState.ts
@@ -8,7 +8,20 @@ export interface AppSettings {
 
 export function defaultAppSettings(): AppSettings {
   return {
-    darkMode: false,
+    darkMode: getDefaultDarkModePreference(),
     threeDViewEnabled: false,
   };
+}
+
+function getDefaultDarkModePreference(): boolean {
+  // Check to see if Media-Queries are supported
+  if (window.matchMedia) {
+    // Check if the dark-mode Media-Query matches
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return false;
 }
